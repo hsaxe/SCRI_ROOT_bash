8c7c8dcd24807bdb729582a0143c9092b0364afc
hsaxe <hsaxe@ucdavis.edu>
2022-04-20 09:55:15 -0700

    Script for getting and combining genome and annotation files.

--- References/rbind_genomes.sh
+++ References/rbind_genomes.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Get genome files (.fna)
+# J. regia
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/411/555/GCF_001411555.2_Walnut_2.0/GCF_001411555.2_Walnut_2.0_genomic.fna.gz
+
+gunzip GCF_001411555.2_Walnut_2.0_genomic.fna.gz
+
+# J. microcarpa
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/785/595/GCF_004785595.1_Jm3101_v1.0/GCF_004785595.1_Jm3101_v1.0_genomic.fna.gz
+
+gunzip GCF_004785595.1_Jm3101_v1.0_genomic.fna.gz
+
+# Get annotation files (.gtf)
+# J. regia
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/411/555/GCF_001411555.2_Walnut_2.0/GCF_001411555.2_Walnut_2.0_genomic.gtf.gz
+
+gunzip GCF_001411555.2_Walnut_2.0_genomic.gtf.gz
+
+# J. microcarpa
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/785/595/GCF_004785595.1_Jm3101_v1.0/GCF_004785595.1_Jm3101_v1.0_genomic.gtf.gz
+
+gunzip GCF_004785595.1_Jm3101_v1.0_genomic.gtf.gz
+
 # Row bind genome files together with cat
 cat GCF_004785595.1_Jm3101_v1.0_genomic.fna GCF_001411555.2_Walnut_2.0_genomic.fna > Jm3101_v1.0_Walnut_2.0_genomic.fna
 
